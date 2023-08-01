4dd68cd4723bed9cb8451b77d5669ecf57badb5f
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-01 16:43:10 +0300

    feat: created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+import Blogs from "./pages/blogs";
 import HomePage from "./pages/home"
+import SignIn from "./pages/sign-in";
+import SignUp from "./pages/sign-up";
+import Profile from "./pages/profile";
 
 import { Container } from "./styles/Container.styled"
 import { GlobalStyles } from "./styles/Global"
+
 
 const App = () => {
   return (
     <>
     <Container>
       <GlobalStyles />
-      <HomePage />
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </Container>
     </>
   )
--- src/layouts/hero/index.tsx
+++ src/layouts/hero/index.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <Title>Keşfetmeye Hazır mısınız?</Title>
         <Subtitle>İlham Veren İçeriklerle Dolu Bir Blog Deneyimi Sizi Bekliyor!</Subtitle>
         <Typography component="p" sx={{  color: "#f4f4f4f4", marginBottom: "0.5em",  fontSize: { xs: "1.1rem", md: "1.4rem" }, fontWeight: 800, lineHeight: 1.5  }}>Blogumuzda ilgi çekici makaleler, bilgi dolu içerikler ve güncel konularla dolu bir dünya sizi bekliyor. Keşfetmek isteyeceğiniz seyahat, teknoloji ve daha birçok konuda derinlemesine analizler sunuyoruz. Bilgiye doyacağınız, eğlenceli ve aydınlatıcı bir okuma deneyimi için hemen başlayın!</Typography>
-        <Link title="Başlayın!" href="/login" rel="noopener noreferrer">Başlayın!</Link>
+        <Link title="Başlayın!" href="/sign-in" rel="noopener noreferrer">Başlayın!</Link>
     </Section>
   )
 }
