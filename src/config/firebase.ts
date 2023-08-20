5ff01ab842596d80e913b2c09d90313837110202
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-20 14:29:19 +0300

    feat: add env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdSwx7GaKEw3dA3zTGgWjBSN3Wtp94mWA",
-  authDomain: "blogcum.firebaseapp.com",
-  projectId: "blogcum",
-  storageBucket: "blogcum.appspot.com",
-  messagingSenderId: "735913223159",
-  appId: "1:735913223159:web:788008dec112ae92b6c4f2",
-  measurementId: "G-TLNLRZ369C",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
